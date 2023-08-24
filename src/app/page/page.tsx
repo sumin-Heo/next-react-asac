d5200f2f2e053495e3ef14c001b9d7f607638159
sumin-Heo <sumin3985@gmail.com>
2023-08-24 10:19:39 +0900

    svg이모지 적용 완료

---
+++ src/app/page/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions';
+
+const Page = () => {
+  return (
+    <div>
+      <h1>이모지</h1>
+      <EmojiEmotionsIcon fontSize="large" />
+    </div>
+  );
+};
+
+export default Page;
